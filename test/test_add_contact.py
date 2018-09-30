964c428352bbd9d0bf020d38488b44b514f6972f
Anastasiia Churkina <evelevel12@gmail.com>
2018-09-30 13:43:13 +0300

    выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,11 +11,11 @@ def app(request):
 
 
 def test_add_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_contact(Contact(firstname="test firstname", middlename="test middlename", lastname="test lastname", nickname="test nickname", title="test title",
                             company="test company", address="test address", homephone="123", mobilephone="123", workphone="123", fax="123", email1="test email1", email2="test email2",
                             email3="test email3", homepage="test homepage", address2="test address", phone2="test phone home", notes="test notes"))
-        app.logout()
+        app.session.logout()
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,14 +12,14 @@ def app(request):
 
 def test_add_group(app):
         app.open_home_page()
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="test", header="test", footer="test"))
-        app.logout()
+        app.session.logout()
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
 
 if __name__ == "__main__":
     unittest.main()
