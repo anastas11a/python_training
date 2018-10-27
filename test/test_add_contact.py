0474f1119c3aa1e969f540d8465eb2a3a3ad76e7
Anastasiia Churkina <evelevel12@gmail.com>
2018-10-27 14:56:16 +0300

    тесты для проверки телефонов, операции со строками

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,7 +6,7 @@ def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="test firstname", middlename="test middlename", lastname="test lastname", nickname="test nickname", title="test title",
                                    company="test company", address="test address", homephone="123", mobilephone="123", workphone="123", fax="123", email1="test email1", email2="test email2",
-                                   email3="test email3", homepage="test homepage", address2="test address", phone2="test phone home", notes="test notes")
+                                   email3="test email3", homepage="test homepage", address2="test address", phone2="1234", notes="test notes")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
