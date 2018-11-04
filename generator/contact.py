c356ecf5eafcf62c594999b3b275467d4bd3b9a7
Anastasiia Churkina <evelevel12@gmail.com>
2018-11-04 14:22:55 +0200

    загрузка тестовых данных из файлов в формате JSON для групп и контактов

--- generator/contact.py
+++ generator/contact.py
@@ -2,9 +2,9 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
 import getopt
 import sys
+import jsonpickle
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
@@ -20,7 +20,6 @@ for o, a in opts:
         n = int(a)
     elif o == "-f":
         f = a
-
 
 
 def random_string(prefix, maxlen):
@@ -45,4 +44,5 @@ testdata = [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent = 2)
+    out.write(jsonpickle.encode(testdata))
