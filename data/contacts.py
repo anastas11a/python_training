f3ce06ceb790cb6aa12e23d38666a0f034587e5f
Anastasiia Churkina <evelevel12@gmail.com>
2018-11-04 13:55:09 +0200

    динамическая генерация тестов для групп и контактов

---
+++ data/contacts.py
@@ -0,0 +1,14 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="name1", lastname="lastname1", middlename="middlename1",
+            nickname="nickname1", title="title1",
+            company="company1", address="address1", homephone="1234",
+            mobilephone="12345", workphone="123456",
+            fax="fax1", email1="email1", email2="email2",
+            email3="email3", homepage="homepage", address2="address2", phone2="1234567",
+            notes="notes"),
+    Contact(firstname="name2", lastname="lastname2", middlename="middlename2")
+]
+
