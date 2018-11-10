a74da0e9e7973021b5d24fe93d3255fefcc44f9d
Anastasiia Churkina <evelevel12@gmail.com>
2018-11-10 16:27:38 +0200

    фикстура для работы с БД + загрузка информации из БД в виде объектов

---
+++ fixture/db.py
@@ -0,0 +1,41 @@
+import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, fax, email, email2, email3, homepage, address2, phone2, notes from addressbook")
+            for row in cursor:
+                (id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, fax, email, email2, email3, homepage, address2, phone2, notes) = row
+                list.append(Contact(id=str(id), firstname=firstname, middlename=middlename, lastname=lastname,
+                                    nickname=nickname, company=company, title=title, address=address, homephone=home, mobilephone=mobile,
+                                    workphone=work, fax=fax, email1=email, email2=email2, email3=email3, homepage=homepage, address2=address2, phone2=phone2, notes=notes))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
